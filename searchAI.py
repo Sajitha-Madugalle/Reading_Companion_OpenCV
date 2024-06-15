d595fb83310f6853561576aa5628bcf4fa6abdbd
Sajitha Madugalle <sajithamadugalle@gmail.com>
2024-06-15 13:42:54 +0530

    Optimized the code, added comments
    removed unused libraries, added comments for better understanding

---
+++ searchAI.py
@@ -0,0 +1,52 @@
+import cv2 as cv
+import os
+from time import sleep
+from windowcapture import WindowCapture
+from text_detection import OCRProcessor
+import google.generativeai as genai
+import pygetwindow as gw
+
+class SearchAI:
+    def __init__(self):
+        # Configure the Gemini API
+        genai.configure(api_key='AIzaSyCiWyhbRpi2FdtQrvI7MmxBtlFECm0ii4Y')
+        self.model = genai.GenerativeModel('gemini-1.0-pro-latest')
+        
+        # Change the working directory to the folder this script is in
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        
+        # Get a list of all open windows
+        self.windows = gw.getAllTitles()
+        self.windows = [win for win in self.windows if win]  # Remove empty window titles
+        
+        self.selected_window = None
+        self.wincap = None
+        self.ocr_processor = None
+
+    def select_window(self, index):
+        self.selected_window = self.windows[index]
+        self.wincap = WindowCapture(self.selected_window)
+
+    def run_once(self):
+        # Get an updated image of the window
+        screenshot = self.wincap.get_screenshot()
+        
+        if screenshot is None or screenshot.size == 0:
+            print("Failed to capture screenshot")
+            return None
+        
+        self.ocr_processor = OCRProcessor(screenshot)
+        text = self.ocr_processor.extract_text()
+        
+        if text.strip():
+            try:
+                # Process the text with Gemini API
+                response = self.model.generate_content(
+                    "find phrases to search in google, to get better understand from this text: " + text
+                )
+                return response.text
+            except Exception as e:
+                print(f"Error processing text with Gemini API: {e}")
+                return f"Error processing text with Gemini API: {e}"
+        else:
+            return "No text extracted from screenshot."
--- windowcapture.py
+++ windowcapture.py
@@ -57,33 +57,19 @@ class WindowCapture:
         win32gui.ReleaseDC(self.hwnd, wDC)
         win32gui.DeleteObject(dataBitMap.GetHandle())
 
-        # drop the alpha channel, or cv.matchTemplate() will throw an error like:
-        #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type() 
-        #   && _img.dims() <= 2 in function 'cv::matchTemplate'
+  
         img = img[...,:3]
 
-        # make image C_CONTIGUOUS to avoid errors that look like:
-        #   File ... in draw_rectangles
-        #   TypeError: an integer is required (got type tuple)
-        # see the discussion here:
-        # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
+
         img = np.ascontiguousarray(img)
 
         return img
 
-    # find the name of the window you're interested in.
-    # once you have it, update window_capture()
-    # https://stackoverflow.com/questions/55547940/how-to-get-a-list-of-the-name-of-every-open-window
     def list_window_names(self):
         def winEnumHandler(hwnd, ctx):
             if win32gui.IsWindowVisible(hwnd):
                 print(hex(hwnd), win32gui.GetWindowText(hwnd))
         win32gui.EnumWindows(winEnumHandler, None)
 
-    # translate a pixel position on a screenshot image to a pixel position on the screen.
-    # pos = (x, y)
-    # WARNING: if you move the window being captured after execution is started, this will
-    # return incorrect coordinates, because the window position is only calculated in
-    # the __init__ constructor.
     def get_screen_position(self, pos):
         return (pos[0] + self.offset_x, pos[1] + self.offset_y)
